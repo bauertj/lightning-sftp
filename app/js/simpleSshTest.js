d0c414d241d695f9ed02c6ca3908dacc7acdcab2
Taylor Bauer <bauertj@plu.edu>
2016-10-24 14:50:42 -0700

    lined up text boxes. Can print info out, working on getting info from forms

--- app/js/simpleSshTest.js
+++ app/js/simpleSshTest.js
@@ -1,9 +1,36 @@
-var SSH = require('simple-ssh');
+//var SSH = require('simple-ssh');
+
+
+function myFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].id + ": "+ x.elements[i].value + "<br>";
+    }
+    document.getElementById("area").innerHTML = text;
+}
+
+
+
+var username, password, server, path1, path2;
+
+//Initializes info from form
+function getInfo() {
+    var x = document.getElementById("frm1");
+
+    username = x.elements[0].value;
+    password = x.elements[1].value;
+    server = x.elements[2].value;
+    path1 = x.elements[3].value;
+    path2 = x.elements[4].value;
+
+}
 
 var ssh = new SSH({
-    host: 'localhost',
-    user: 'username',
-    pass: 'password'
+    host: server,
+    user: username,
+    pass: password
 });
 
 ssh.exec('echo $PATH', {
