729991b2ec3f086383873191961cf7917d3aa804
Taylor Bauer <bauertj@plu.edu>
2016-10-24 10:53:02 -0700

    Added css

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var mainWindow = null;
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         height: 600,
-        width: 800
+        width: 800,
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
