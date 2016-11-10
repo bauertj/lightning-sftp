a569b3bfdfdd23784c60f4c2a588f251b0e75a31
Taylor Bauer <bauertj@plu.edu>
2016-11-10 15:55:31 -0800

    remove all / remove selected options work in connection window. Changed settings of main window/history window. Added new connection window under file (does not work completely yet)

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
-
+var {Menu} = require('electron');
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
+        height: 900,
+        width: 1500,
         fullscreenable: false,
         title: "Lightning SFTP"
     });
@@ -17,6 +17,66 @@ app.on('ready', function() {
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
 
+const template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Connection',
+                accelerator: 'CmdOrCtrl+N',
+                click() {
+                    var connectionWindow = new BrowserWindow({
+                        height: 300,
+                        width: 350,
+                        alwaysOnTop: true,
+                        resizable: false
+                    });
+
+                    connectionWindow.loadURL('file://' + __dirname + '/app/connectionWindow.html');
+
+                    connectionWindow.on('closed', function () {
+                        connectionWindow = null;
+                    });
+                }
+            },
+            {
+                label: 'Bookmarks',
+                accelerator: 'CmdOrCtrl+B',
+                click() {
+                    var bookmarksWindow = new BrowserWindow({
+
+                    });
+                }
+            },
+            {
+                label: 'Exit',
+                role: 'close'
+            }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click (item, focusedWindow) {
+                    if (focusedWindow) focusedWindow.reload()
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+                click (item, focusedWindow) {
+                    if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+                }
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 const {ipcMain} = require('electron');
 ipcMain.on('close-main-window', function () {
@@ -33,7 +93,8 @@ ipcMain.on('open-history-window', function () {
 
     historyWindow = new BrowserWindow({
         height: 500,
-        width: 1024
+        width: 1024,
+        alwaysOnTop: true
     });
 
     historyWindow.loadURL('file://' + __dirname + '/app/historyWindow.html');
