65bcfd7e483b685e5bb77d49e19433a24b6a3701
lightning-sftp <bauertj@plu.edu>
2017-03-17 12:18:17 -0700

    can change root with drop down on local tree

---
+++ app/js/moreScripts.js
@@ -0,0 +1,8 @@
+$('#upperLevelsLocal').change(function(){
+    somepath = $(this)[0].value;
+    var newJson = setTree(somepath);
+
+    $('#jstree').jstree('destroy');
+
+    initTree(newJson);
+})
